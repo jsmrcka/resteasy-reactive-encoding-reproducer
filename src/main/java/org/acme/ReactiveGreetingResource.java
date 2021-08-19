4f1f1d3850edd8c69f03012747c1b42a694c983e
Josef Smrcka <jsmrcka@redhat.com>
2021-08-19 19:19:20 +0200

    add echo endpoint

--- src/main/java/org/acme/ReactiveGreetingResource.java
+++ src/main/java/org/acme/ReactiveGreetingResource.java
@@ -1,16 +1,23 @@
 package org.acme;
 
-import org.jboss.resteasy.reactive.MultipartForm;
-
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.resteasy.reactive.MultipartForm;
+
 @Path("/multipart")
 public class ReactiveGreetingResource {
+
+    @POST
+    @Path("/echo")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String echo(String request) {
+        return request;
+    }
 
     @POST
     @Path("/text")
