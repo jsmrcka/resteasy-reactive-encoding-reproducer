7f2685fee467666866da6d48e74dffcd9f43faf6
Josef Smrcka <jsmrcka@redhat.com>
2021-08-20 12:05:44 +0200

    change sample text

--- src/test/java/org/acme/EchoResourceTest.java
+++ src/test/java/org/acme/EchoResourceTest.java
@@ -15,11 +15,13 @@ import io.restassured.specification.MultiPartSpecification;
 
 @QuarkusTest
 public class EchoResourceTest {
-    private static final byte[] TEXT_WITH_DIACRITICS_BYTES = new byte[] { 80, -59, -103, 105, 107, 114, -61, -95, -59, -95, 108,
-            101, 110, -61, -67, 32, -59, -66, 108, 111, -59, -91, 111, 117, -60, -115, 107, -61, -67, 32, 107, -59, -81, -59,
-            -120, 32, -61, -70, 112, -60, -101, 108, 32, -60, -113, -61, -95, 98, 101, 108, 115, 107, -61, -87, 32, -61, -77,
-            100, 121, 46 };
-    // "Přikrášlený žloťoučký kůň úpěl ďábelské ódy."
+    private static final byte[] TEXT_WITH_DIACRITICS_BYTES = new byte[] { 75, -59, -107, 100, 101, -60, -66, 32, -59, -95, -59,
+            -91, 97, 115, 116, 110, -61, -67, 99, 104, 32, -60, -113, 97, 116, -60, -66, 111, 118, 32, 117, -60, -115, -61, -83,
+            32, 112, 114, 105, 32, -61, -70, 115, 116, -61, -83, 32, 86, -61, -95, 104, 117, 32, 109, -60, -70, 107, 118, 101,
+            104, 111, 32, 107, 111, -59, -120, 97, 32, 111, 98, 104, 114, -61, -67, 122, 97, -59, -91, 32, 107, -61, -76, 114,
+            117, 32, 97, 32, -59, -66, 114, 97, -59, -91, 32, -60, -115, 101, 114, 115, 116, 118, -61, -87, 32, 109, -61, -92,
+            115, 111, 46 };
+    // "Kŕdeľ šťastných ďatľov učí pri ústí Váhu mĺkveho koňa obhrýzať kôru a žrať čerstvé mäso."
     private static final String TEXT_WITH_DIACRITICS = new String(TEXT_WITH_DIACRITICS_BYTES, StandardCharsets.UTF_8);
 
     @Test
